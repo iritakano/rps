50f0f477409608a4756eb171202dcd743e9b4122
Iri Takano <iri2121@icloud.com>
2025-11-13 00:03:56 -0500

    feat: add playRound function that compares humanChoice to computerChoice and returns console.log message accordingly

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
 function getComputerChoice() {
     let choices = ["rock", "paper", "scissors"];
     let choicesIndex = Math.floor(Math.random() * choices.length);
-    let computerChoice = choices[choicesIndex];
-    return computerChoice;
+    return choices[choicesIndex];
 }
 
 function getHumanChoice() {
-    let humanChoice = prompt("Choose rock, paper, or scissors: ");
+    return prompt("Choose rock, paper, or scissors: ");
 }
 
 let humanScore = 0;
 let computerScore = 0;
 
+function playRound(humanChoice, computerChoice){
+    humanChoice = humanChoice.toLowerCase();
+    if (humanChoice == computerChoice){
+        console.log(`You chose ${humanChoice}. Computer chose ${computerChoice}. It's a tie.`);
+    }
+
+    else if((humanChoice == "rock" && computerChoice == "paper") ||
+    (humanChoice == "paper" && computerChoice == "scissors") ||
+    (humanChoice == "scissors" && computerChoice == "rock")){
+        console.log(`You chose ${humanChoice}. Computer chose ${computerChoice}. You lose.`);
+        humanScore += 1;
+    }
+
+    else{
+        console.log(`You chose ${humanChoice}. Computer chose ${computerChoice}. You win!`);
+        computerScore += 1;
+    }
+}
+
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
