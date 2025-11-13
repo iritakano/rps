87d2c61fe3ac364e207be50789041b1592e807a0
Iri Takano <iri2121@icloud.com>
2025-11-13 00:23:32 -0500

    feat: add playGame function to call playRound 5 times and console.log result. feat:added console.log on playRound to show computer and human scores for each round.

--- script.js
+++ script.js
@@ -14,24 +14,49 @@ let computerScore = 0;
 function playRound(humanChoice, computerChoice){
     humanChoice = humanChoice.toLowerCase();
     if (humanChoice == computerChoice){
-        console.log(`You chose ${humanChoice}. Computer chose ${computerChoice}. It's a tie.`);
+        console.log(`You chose ${humanChoice}. Computer chose ${computerChoice}. It's a tie this round.`);
+        console.log(`Your score: ${humanScore}`);
+        console.log(`Computer score: ${computerScore}`);
     }
 
     else if((humanChoice == "rock" && computerChoice == "paper") ||
     (humanChoice == "paper" && computerChoice == "scissors") ||
     (humanChoice == "scissors" && computerChoice == "rock")){
-        console.log(`You chose ${humanChoice}. Computer chose ${computerChoice}. You lose.`);
-        humanScore += 1;
+        console.log(`You chose ${humanChoice}. Computer chose ${computerChoice}. You lose the round.`);
+        computerScore += 1;
+        console.log(`Your score: ${humanScore}`);
+        console.log(`Computer score: ${computerScore}`);
     }
 
     else{
-        console.log(`You chose ${humanChoice}. Computer chose ${computerChoice}. You win!`);
-        computerScore += 1;
+        console.log(`You chose ${humanChoice}. Computer chose ${computerChoice}. You win the round!`);
+        humanScore += 1;
+        console.log(`Your score: ${humanScore}`);
+        console.log(`Computer score: ${computerScore}`);
     }
 }
 
+function playGame() {
+    let round = 1;
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+    for(round = 1; round <= 5; round++){
+        console.log(`Round ${round}`);
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+    }
 
-playRound(humanSelection, computerSelection);
+    if (humanScore > computerScore){
+        console.log("You win the game!")
+    }
+
+    else if (humanScore == computerScore){
+        console.log("You tied this game.")
+    }
+
+    else {
+        console.log("You lose the game :(")
+    }
+}
+
+playGame();
